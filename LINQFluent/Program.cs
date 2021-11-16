1f35255900496a782a3c549cb73da4b347c92cc0
Pectron <pedropassos.dev@gmail.com>
2021-11-16 12:57:47 +0000

    LINQ

---
+++ LINQFluent/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LINQFluent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] texto = File.ReadAllLines("livro.txt");
+
+            int linhasCompMaior30 = texto.Count(linha => linha.Length > 30);
+            Console.WriteLine(linhasCompMaior30);
+
+            double mediaCaracteres = texto.Average( linha => linha.Length );
+            Console.WriteLine(mediaCaracteres);
+
+            bool linhaMais = texto.Any( linhas => linhas.Length > 120 );
+            Console.WriteLine(linhaMais);
+
+            IEnumerable<string> ComecaY = texto.Where( linha => linha.Contains("Y") )
+                                                .Select( linha => linha.Split()?[0].ToUpper());
+            priprintarnt(ComecaY);
+        }
+
+
+        static void priprintarnt(IEnumerable<string> lista)
+        {
+            foreach(string s in lista)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+        }
+    }
+}
