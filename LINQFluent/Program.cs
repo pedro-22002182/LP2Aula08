ec6aff8a189a54ded514067426473a35044f2ca5
Pectron <pedropassos.dev@gmail.com>
2021-11-16 13:09:09 +0000

    LINQ

--- LINQFluent/Program.cs
+++ LINQFluent/Program.cs
@@ -14,15 +14,41 @@ namespace LINQFluent
             int linhasCompMaior30 = texto.Count(linha => linha.Length > 30);
             Console.WriteLine(linhasCompMaior30);
 
+            linhasCompMaior30 = 
+                            (from linha in texto
+                            where linha.Length > 30
+                            select linha).Count();
+
+
+
             double mediaCaracteres = texto.Average( linha => linha.Length );
             Console.WriteLine(mediaCaracteres);
+
+            mediaCaracteres =
+                            (from linha in texto
+                            select linha.Length).Average();
+
+
 
             bool linhaMais = texto.Any( linhas => linhas.Length > 120 );
             Console.WriteLine(linhaMais);
 
+            linhaMais =
+                            (from linha in texto
+                            where linha.Length > 120
+                            select linha).Any();
+
+
+
             IEnumerable<string> ComecaY = texto.Where( linha => linha.Contains("Y") )
                                                 .Select( linha => linha.Trim().Split()?[0].ToUpper());
             priprintarnt(ComecaY);
+
+
+            linhaMais =
+                            (from linha in texto
+                            where linha.Contains("Y")
+                            select linha => linha.Trim().Split()?[0].ToUpper());
         }
 
 
